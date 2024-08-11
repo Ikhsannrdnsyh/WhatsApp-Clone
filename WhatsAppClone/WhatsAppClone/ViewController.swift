a19d544ba8e0fc8fc483a9909302f78998f3cfca
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-08-11 18:12:34 +0700

    create new project

---
+++ WhatsAppClone/WhatsAppClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 11/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
