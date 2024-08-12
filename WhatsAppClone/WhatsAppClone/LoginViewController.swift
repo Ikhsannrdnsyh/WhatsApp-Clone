fc5d51dfce87873010b1461f16ad93e961d7f497
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-08-12 14:16:20 +0700

    implement UI Login and Register

---
+++ WhatsAppClone/WhatsAppClone/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  WhatsAppClone
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 12/08/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
