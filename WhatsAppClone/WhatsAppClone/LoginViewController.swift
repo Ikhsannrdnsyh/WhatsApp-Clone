0db660054c93d5140a178fbd8b7e38c8dfd39298
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-08-12 14:36:59 +0700

    IBOutlet connection

--- WhatsAppClone/WhatsAppClone/LoginViewController.swift
+++ WhatsAppClone/WhatsAppClone/LoginViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    //MARK: - IBOutlets
+    //Label
+    @IBOutlet weak var registerLabel: UILabel!
+    
+    //Textfield
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    //Button
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    //MARK: - IBActions
+    @IBAction func loginButtonTap(_ sender: Any) {
+        print("loginButtonTap")
+    }
+    
+    @IBAction func forgotPasswordButtonTp(_ sender: Any) {
+        print("forgotPassword")
+    }
+    
+    @IBAction func registerButtonTap(_ sender: Any) {
+        print("registerButton")
+    }
 }
 
