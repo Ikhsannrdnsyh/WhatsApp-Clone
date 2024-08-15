3f4c84118dba42092ec0d276b8f9a2e47bda6379
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-08-15 19:18:53 +0700

    Edit tab setting and edit profile, edit storyboard

--- WhatsAppClone/WhatsAppClone/View/SettingsView/SettingsTableViewController.swift
+++ WhatsAppClone/WhatsAppClone/View/SettingsView/SettingsTableViewController.swift
@@ -28,6 +28,15 @@ class SettingsTableViewController: UITableViewController{
         fetchUserInfo()
     }
     
+    //MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            performSegue(withIdentifier: "editProfileSegue", sender: self)
+        }
+    }
+    
     //MARK: - IBAction
     
     @IBAction func tellAFriendButtonTap(_ sender: Any) {
