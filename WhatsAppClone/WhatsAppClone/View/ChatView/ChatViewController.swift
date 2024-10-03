6fd3739bdd2a1572307566792e328216eed9dbb7
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-10-03 10:52:54 +0700

    create chat view controller

---
+++ WhatsAppClone/WhatsAppClone/View/ChatView/ChatViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ChatViewController.swift
+//  WhatsAppClone
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 03/10/24.
+//
+
+import UIKit
+import MessageKit
+
+class ChatViewController: MessagesViewController {
+    
+    //MARK: - Vars
+    
+    private var chatId = ""
+    private var recipientId = ""
+    private var recipientName = ""
+    private var recipientAvatar = ""
+
+   
+    //MARK: - Inits
+    
+    init(chatId: String = "", recipientId: String = "", recipientName: String = "", recipientAvatar: String = "") {
+        super.init(nibName: nil, bundle: nil)
+        self.chatId = chatId
+        self.recipientId = recipientId
+        self.recipientName = recipientName
+        self.recipientAvatar = recipientAvatar
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    //MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- WhatsAppClone/WhatsAppClone/View/RecentChatsView/RecentChatTableViewController.swift
+++ WhatsAppClone/WhatsAppClone/View/RecentChatsView/RecentChatTableViewController.swift
@@ -82,7 +82,7 @@ class RecentChatTableViewController: UITableViewController {
         FirebaseRecentChatListener.shared.clearUnreadCounter(recentChat: recentChat)
         
         //navigate to chat room
-        
+        navigateToChatRoom(recent: recentChat)
     }
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -116,6 +116,14 @@ class RecentChatTableViewController: UITableViewController {
             return recentChat.receiverName.lowercased().contains(text.lowercased())
         })
         tableView.reloadData()
+    }
+    
+    private func navigateToChatRoom(recent: RecentChat){
+        StartChatHelper.shared.restartChat(chatRoomId: recent.chatRoomId, memberIds: [recent.senderId, recent.receiverID])
+        
+        let chatVC = ChatViewController(chatId: recent.chatRoomId,recipientId: recent.receiverID,recipientName: recent.receiverName,recipientAvatar: recent.avatar)
+        
+        navigationController?.pushViewController(chatVC, animated: true)
     }
 }
 
